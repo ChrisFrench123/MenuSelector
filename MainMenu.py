3a1d64b59402eb8d66e7e320dbd625236d8b4b62
ChrisFrench123 <chrisfrenchtoaster@gmail.com>
2020-06-30 15:25:41 +0100

    cleaned up and commented

--- MainMenu.py
+++ MainMenu.py
@@ -11,6 +11,7 @@ MaxLine = len(CurrentMenu) - 1
 NoOfLines = len(CurrentMenu)
 
 
+# get the current key being pressed
 def on_press(key):
     current_key = key
     if str(current_key) == "Key.down" or str(current_key)== "Key.up":
@@ -19,41 +20,48 @@ def on_press(key):
         select_line()
 
 
+# stop the program when esc is pressed
 def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
 
+# changing the current line
 def move_selector(current_key):
     global CurrentLine
     global MaxLine
     if str(current_key) == "Key.down" and CurrentLine < MaxLine:
         CurrentLine += 1
-
     if str(current_key) == "Key.up" and CurrentLine > 0:
         CurrentLine -= 1
 
     draw_menu()
 
 
+# what happens when you select a line
 def select_line():
     global CurrentMenu
     print("current line is ", CurrentLine, "and line name is ", CurrentMenu[CurrentLine])
+
+    # here is where the actions of selecting the line goes
     if CurrentMenu[CurrentLine] == "line 7":
         CurrentMenu = FirstMenu
         print("it is now the first menu")
         draw_menu()
+    elif CurrentMenu[CurrentLine] == "line 0":
+        CurrentMenu = SecondMenu
+        print("it is now the first menu")
+        draw_menu()
 
 
+# draw the menu to the screen
 def draw_menu():
     print("\n" * 10)
     system('cls')
-
     for i in range(NoOfLines):
         if CurrentLine == i:
             print("[", CurrentMenu[i], "]")
-
         else:
             print(CurrentMenu[i])
 
